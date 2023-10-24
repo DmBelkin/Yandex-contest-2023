41bcec1090d2b17593b7ecb78a69611541c620e1
DmBelkin <143951259+DmBelkin@users.noreply.github.com>
2023-10-25 02:05:29 +0300

    with yandex cup

--- src/main/java/Hokku.java
+++ src/main/java/Hokku.java
@@ -6,16 +6,8 @@ import java.nio.file.Paths;
 
 public class Hokku {
 
-    public static void main(String[] args) {
-        try {
-            File file = hokku(new File("input.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-
-    public static File hokku(File file) throws IOException {
+    public File hokku(File file) throws IOException {
         File resultFile = new File("out/output7.txt");
         PrintWriter writer = new PrintWriter(resultFile);
         String input = Files.readString(Paths.get(file.getPath()));
@@ -31,17 +23,12 @@ public class Hokku {
         return resultFile;
     }
 
-    public static String getResult(String str, String input) {
+    public String getResult(String str, String input) {
         String result = "";
         for (int i = 0; i < str.length(); i++) {
             String symbol = "" + str.charAt(i);
             if (input.contains(symbol)) {
                 int index = input.indexOf(symbol);
-                for (int j = 0; j < str.length(); j++) {
-                    if (input.contains("" + str.charAt(j))) {
-                        input.replace("" + str.charAt(j), "");
-                    }
-                }
                 result += input.substring(0, index - i);
                 result += str;
                 result += input.substring(index + str.length() - i);
