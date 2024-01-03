eda65048637780766a626b78b0df07f2dbddd790
DmBelkin <143951259+DmBelkin@users.noreply.github.com>
2024-01-03 21:12:27 +0300

    all permutations

--- src/main/java/AllPermutations.java
+++ src/main/java/AllPermutations.java
@@ -1,15 +1,17 @@
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllPermutations {
 
+    private List<String> list;
 
-
-    public static File permutation(File file) throws IOException {
+    public File permutation(File file) throws IOException {
         File resultFile = new File("out/output40.txt");
         /**
          * решена до значения 4
@@ -17,69 +19,32 @@ public class AllPermutations {
         FileWriter writer = new FileWriter(resultFile);
         String st = Files.readString(Paths.get(file.getPath())).trim();
         int num = Integer.parseInt(st);
-        int factorial = 1;
         String str = "";
         for (int i = 1; i <= num; i++) {
-            factorial *= i;
             str += i;
         }
-        if (str.length() == 1) {
-            writer.write(Integer.toString(1));
-        } else {
-            int[] ary = new int[str.length()];
-            for (int i = 0; i < str.length(); i++) {
-                ary[i] = Integer.parseInt("" + str.charAt(i));
-            }
-            int[][] res = new int[(str.length())][str.length() - 1];
-            int s = 0;
-            for (int j = 0; j < res.length; j++) {
-                int ind = 0;
-                for (int i = 0; i < ary.length; i++) {
-                    if (ary[s] == i + 1) {
-                        continue;
-                    }
-                    res[j][ind++] = ary[i];
-                }
-                s++;
-            }
-            StringBuilder builder = new StringBuilder();
-            for (int k = 0; k < res.length; k++) {
-                builder.append(ary[k]);
-                int length = res[k].length;
-                int[] ar = res[k];
-                String result = Arrays.toString(ar);
-                builder.append(result.substring(1, result.length() - 1).
-                        replaceAll(",\s", "") + "\n");
-                for (int m = length; m > 0; m--) {
-                    for (int i = length - 1; i > 0 ; i--) {
-                        if (ar[i - 1] < ar[i]) {
-                            int pivot = i;
-                            for (int j = pivot; j < length; j++) {
-                                if (ar[j] <= ar[pivot] && ar[i - 1] < ar[j]) {
-                                    pivot = j;
-                                }
-                            }
-                            int tmp = ar[i - 1];
-                            ar[i - 1] = ar[pivot];
-                            ar[pivot] = tmp;
-                            for (int j = pivot; j < length; j++) {
-                                int tmp1 = ar[j];
-                                ar[j] = ar[i];
-                                ar[i] = tmp1;
-                            }
-                            result = Arrays.toString(ar);
-                            builder.append(ary[k]);
-                            builder.append(result.substring(1, result.length() - 1).
-                                    replaceAll(",\s", "") + "\n");
-                        }
-                    }
-                }
-            }
-            String r = builder.deleteCharAt(builder.length() - 1).toString();
-            writer.write(r);
+        list = new ArrayList<>();
+        permutation("", str);
 
+        String res = "";
+        for(String s : list) {
+            res += s + "\n";
         }
+        writer.write(res.trim());
         writer.close();
         return resultFile;
+    }
+
+    public void permutation(String prefix, String str) {
+        int n = str.length();
+        if (n == 0) {
+            if (!list.contains(prefix)) {
+                list.add(prefix);
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+            }
+        }
     }
 }
--- src/main/java/FinalContest.java
+++ src/main/java/FinalContest.java
@@ -10,10 +10,32 @@ public class FinalContest {
 
 
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        List<Integer> first = new ArrayList<>();
-        List<Integer> second = new ArrayList<>();
+        solution(16);
+        //треугольник паскаля
 
+    }
+
+    public static List<Integer> solution(Integer s) {
+        List<Integer> list = new ArrayList<>();
+        int fn = factorial(s);
+        int sum = 0;
+        System.out.println(fn);
+        for (int i = 0;s - i > 0; i++) {
+            int num = fn / (factorial(i) * factorial(s - i));
+            sum += num;
+            System.out.println(num);
+            list.add(num);
+        }
+        list.add(1);
+        return list;
+
+    }
+
+    public static int factorial(int n) {
+        int res = 1;
+        for (int i = 1; i <=n; i++) {
+            res *= i;
+        }
+        return res;
     }
 }
